368464ba9a11dee43f51da33cde3493c63e16217
nadyakit <nadezda.sysol2@mail.ru>
2021-06-25 02:41:10 +0300

    Create hover_main.js

---
+++ js/hover_main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.events_first').hover(
+      function(){
+          $('.h3_otstup').css('color','#FF3F3D');
+      }
+  )
+});
+
+//
+// $(document).ready(function() {
+//   $('.events_first').hover(
+//     function mouseOver() {
+//        document.getElementByClass('.h3_otstup').style.color = '#FF3F3D';
+//        console.log("In");
+//      },
+//
+//      function mouseOut() {
+//        document.getElementByClass('.h3_otstup').style.color = '#602D91';
+//        console.log("Out");
+//      }
+//   )
+// });
