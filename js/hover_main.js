62b9353da2e8c4df267d9741b264e99903d0bf57
nadyakit <nadezda.sysol2@mail.ru>
2021-06-25 02:53:55 +0300

    Update hover_main.js

--- js/hover_main.js
+++ js/hover_main.js
@@ -7,16 +7,31 @@
 // });
 
 
-$(document).ready(function() {
-  $('.events_first').hover(
-    function mouseOver() {
-       document.getElementByClass('.h3_otstup').style.color = '#FF3F3D';
-       console.log("In");
-     },
+// $(document).ready(function() {
+//   $('.events_first').hover(
+//     function mouseOver() {
+//        document.getElementByClass('.h3_otstup').style.color = '#FF3F3D';
+//        console.log("In");
+//      },
+//
+//      function mouseOut() {
+//        document.getElementByClass('.h3_otstup').style.color = '#602D91';
+//        console.log("Out");
+//      }
+//   )
+// });
 
-     function mouseOut() {
-       document.getElementByClass('.h3_otstup').style.color = '#602D91';
-       console.log("Out");
-     }
-  )
+$(document).ready(function() {
+  $('.events_first').hover(function(){
+    $('.h3_otstup').css('color', '#FF3F3D');
+  }),
+  $('.events_first').mouseout(function(){
+    $('.h3_otstup').css('color', '#602D91');
+  }),
+  $('.events_first').hover(function(){
+    $('.date').css('color', '#FF3F3D');
+  }),
+  $('.events_first').mouseout(function(){
+    $('.date').css('color', '#602D91');
+  })
 });
