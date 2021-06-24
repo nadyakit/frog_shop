062a83c90cde1f066b58a26b057201b35958d6d3
nadyakit <nadezda.sysol2@mail.ru>
2021-06-25 00:44:32 +0300

    Update button_zayavka.js

--- js/button_zayavka.js
+++ js/button_zayavka.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
       function(){
           $('.z_ready').css('visibility', 'visible');
   })
-})
+});
+
+$(document).ready(function(){
+  $('.button_zayavka').click(
+    function(){
+      $('.button_zayavka').css('animation-play-state','running')
+    }
+  )
+});
